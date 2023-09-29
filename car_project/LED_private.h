8fa6750dcfc9da352788c85cc4c1740ca54f50ba
ahmed atia <77337808+Ahmed1010-1999@users.noreply.github.com>
2023-09-29 09:04:39 +0300

    Update LED_private.h

--- car_project/LED_private.h
+++ car_project/LED_private.h
@@ -1,8 +1,4 @@
-/*********************************************************/
-/* Author  : Abdelrahman Mohamed              			 */
-/* Date     : 4 Jan	2022                   				 */
-/* Version  : V01                            			 */
-/*********************************************************/
+
 
 #ifndef LED_PRIVATE_H
 #define LED_PRIVATE_H
