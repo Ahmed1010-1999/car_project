4b576efeb2e16909282dc08332bbd7e4ce27c913
ahmed atia <77337808+Ahmed1010-1999@users.noreply.github.com>
2023-09-29 09:05:32 +0300

    Update Interrupt_interface.h

--- car_project/Interrupt_interface.h
+++ car_project/Interrupt_interface.h
@@ -1,8 +1,4 @@
-/*********************************************************/
-/* Author  : Abdelrahman Mohamed              			 */
-/* Date     : 6 Jan	2022                   				 */
-/* Version  : V01                            			 */
-/*********************************************************/
+
 
 #ifndef INTERRUPT_INTERFACE_H
 #define INTERRUPT_INTERFACE_H
